5137832982cb808c16bc34d056f119d936be438c
Bharath Vitala <86616810+VB2004@users.noreply.github.com>
2025-10-24 20:39:24 +0530

    fix

--- user-data-scripts/user-data.sh
+++ user-data-scripts/user-data.sh
@@ -7,7 +7,7 @@ BRANCH="main"
 APP_DIR="/opt/app"
 APP_PORT=3000
 # TODO: REPLACE THE IP BELOW
-LOKI_URL="http://10.0.7.183:3100/loki/api/v1/push"
+LOKI_URL="http://10.0.20.146:3100/loki/api/v1/push"
 PROMTAIL_VERSION="2.8.8"
 
 # ========== UPDATE & BASICS ==========
@@ -24,7 +24,7 @@ mkdir -p /etc/apt/keyrings
 curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
 
 NODE_VERSION="20"  # LTS version, change if needed
-echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_VERSION.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
 
 apt-get update -y
 apt-get install -y nodejs
